163e60a8054007244521a2d64b6488836b183129
Manuel Seitz <136127838+ManuelSeitz@users.noreply.github.com>
2023-07-29 19:17:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const menuIcon = document.getElementById('menuIcon');
+
+menuIcon.addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    const blackScreen = document.getElementById('blackScreen');
+    if (menuIcon.ariaExpanded === 'false') {
+        menuIcon.ariaExpanded = true;
+        menuIcon.src = '/assets/images/icon-menu-close.svg';
+        menuIcon.width = '32';
+        menu.classList.replace('translate-x-full', 'translate-x-0');
+        blackScreen.classList.remove('hidden');
+    } else {
+        menuIcon.ariaExpanded = false;
+        menuIcon.src = '/assets/images/icon-menu.svg';
+        menuIcon.width = '40';
+        menu.classList.replace('translate-x-0', 'translate-x-full');
+        blackScreen.classList.add('hidden');
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        softOrange: 'hsl(35, 77%, 62%)',
+        softRed: 'hsl(5, 85%, 63%)',
+        offWhite: 'hsl(36, 100%, 99%)',
+        grayishBlue: 'hsl(233, 8%, 79%)',
+        darkGrayishBlue: 'hsl(236, 13%, 42%)',
+        veryDarkBlue: 'hsl(240, 100%, 5%)',
+      },
+    },
+  },
+  plugins: [],
+}
+
