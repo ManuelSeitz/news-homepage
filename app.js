5ceb430d6a235cc9a8d559c5d5bdf89f048c6f3c
Manuel Seitz <136127838+ManuelSeitz@users.noreply.github.com>
2023-07-29 19:35:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ menuIcon.addEventListener('click', () => {
     const blackScreen = document.getElementById('blackScreen');
     if (menuIcon.ariaExpanded === 'false') {
         menuIcon.ariaExpanded = true;
-        menuIcon.src = '/assets/images/icon-menu-close.svg';
+        menuIcon.src = 'assets/images/icon-menu-close.svg';
         menuIcon.width = '32';
         menu.classList.replace('translate-x-full', 'translate-x-0');
         blackScreen.classList.remove('hidden');
     } else {
         menuIcon.ariaExpanded = false;
-        menuIcon.src = '/assets/images/icon-menu.svg';
+        menuIcon.src = 'assets/images/icon-menu.svg';
         menuIcon.width = '40';
         menu.classList.replace('translate-x-0', 'translate-x-full');
         blackScreen.classList.add('hidden');
